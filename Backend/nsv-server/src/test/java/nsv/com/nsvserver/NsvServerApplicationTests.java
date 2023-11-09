9261cb7270f444a7a17e8fc5297d5a0838953743
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2023-11-09 21:50:40 +0700

    init backend server

---
+++ Backend/nsv-server/src/test/java/nsv/com/nsvserver/NsvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nsv.com.nsvserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
