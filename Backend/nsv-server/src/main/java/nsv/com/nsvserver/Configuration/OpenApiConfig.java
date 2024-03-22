79b01ad2f209c85516cf318b8db16555017ce7e1
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2024-03-22 15:36:43 +0700

    config url using for swagger ui call

--- Backend/nsv-server/src/main/java/nsv/com/nsvserver/Configuration/OpenApiConfig.java
+++ Backend/nsv-server/src/main/java/nsv/com/nsvserver/Configuration/OpenApiConfig.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "NSV API", version = "v1"))
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")},info = @Info(title = "NSV API", version = "v1"))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
