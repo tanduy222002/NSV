489d7e2c23f6c6a07516ad4456f8031519e319ce
TheRoamer-2901 <dangkhoa29012002@gmail.com>
2024-04-10 15:35:53 +0700

    mobile client init

---
+++ Frontend/mobile/App.jsx
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LoginScreen } from "./screens";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+
+      {/* <View style={styles.container}>
+        <Text className="font-semibold text-red-500">
+          Open up App.js to start working on your project!
+        </Text>
+        <StatusBar style="auto" />
+      </View> */}
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Frontend/mobile/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text className="text-blue-600 font-semibold">Login</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
