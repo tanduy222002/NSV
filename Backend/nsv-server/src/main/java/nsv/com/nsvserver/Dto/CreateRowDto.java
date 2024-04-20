3116b916be097f25bd1872a481852521d1f978c1
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2024-04-20 09:30:07 +0700

    add capacity when create slot in map

--- Backend/nsv-server/src/main/java/nsv/com/nsvserver/Dto/CreateRowDto.java
+++ Backend/nsv-server/src/main/java/nsv/com/nsvserver/Dto/CreateRowDto.java
@@ -2,6 +2,7 @@ package nsv.com.nsvserver.Dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,8 @@ public class CreateRowDto {
     private String name;
     @JsonProperty("y_position")
     private int yPosition;
+    @NotNull(message = "capacity is mandatory")
+    private Double capacity;
     @Valid
     private List<CreateSlotDto> slotDtos;
 
