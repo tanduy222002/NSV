69960b8b3003fa5cdd0d1bc7be8912a1a8ffde51
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2024-05-05 11:11:35 +0700

    git feat create export ticket

--- Backend/nsv-server/src/main/java/nsv/com/nsvserver/Service/TransferTicketService.java
+++ Backend/nsv-server/src/main/java/nsv/com/nsvserver/Service/TransferTicketService.java
@@ -5,7 +5,7 @@ import nsv.com.nsvserver.Dto.*;
 import nsv.com.nsvserver.Entity.*;
 import nsv.com.nsvserver.Exception.BinWeightMismatchException;
 import nsv.com.nsvserver.Exception.NotFoundException;
-import nsv.com.nsvserver.Exception.SlotOverContaining;
+import nsv.com.nsvserver.Exception.SlotAreaMismatchException;
 import nsv.com.nsvserver.Exception.TicketStatusMismatchException;
 import nsv.com.nsvserver.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,8 +30,10 @@ public class TransferTicketService {
 
     BinSlotRepository binSlotRepository;
 
+    BinDao binDaoImpl;
+
     @Autowired
-    public TransferTicketService(QualityRepository qualityRepository, SlotRepository slotRepository, TransferTicketRepository transferTicketRepository, PartnerRepository partnerRepository, EmployeeRepository employeeRepository, TicketDao ticketDaoImpl, BinSlotRepository binSlotRepository) {
+    public TransferTicketService(QualityRepository qualityRepository, SlotRepository slotRepository, TransferTicketRepository transferTicketRepository, PartnerRepository partnerRepository, EmployeeRepository employeeRepository, TicketDao ticketDaoImpl, BinSlotRepository binSlotRepository, BinDao binDaoImpl) {
         this.qualityRepository = qualityRepository;
         this.slotRepository = slotRepository;
         this.transferTicketRepository = transferTicketRepository;
@@ -39,22 +41,23 @@ public class TransferTicketService {
         this.employeeRepository = employeeRepository;
         this.ticketDaoImpl = ticketDaoImpl;
         this.binSlotRepository = binSlotRepository;
+        this.binDaoImpl = binDaoImpl;
     }
 
     @Transactional
-    public String createImportTransferTicket(CreateTransferTicketDto createTransferTicketDto){
-        List<CreateBinDto> binDtos =createTransferTicketDto.getBinDto();
-        CreateDebtDto createDebtDto =createTransferTicketDto.getDebtDto();
+    public String createImportTransferTicket(CreateTransferTicketDto importTicketDto){
+        List<CreateBinDto> binDtos =importTicketDto.getBinDto();
+        CreateDebtDto createDebtDto =importTicketDto.getDebtDto();
         Employee employee = EmployeeDetailService.getCurrentUserDetails();
         employee= employeeRepository.findById(employee.getId()).orElseThrow(()->new NotFoundException("No employee found"));
         TransferTicket transferTicket = new TransferTicket();
         transferTicket.setType("IMPORT");
-        transferTicket.setName(createTransferTicketDto.getName());
-        transferTicket.setDescription(createTransferTicketDto.getDescription());
+        transferTicket.setName(importTicketDto.getName());
+        transferTicket.setDescription(importTicketDto.getDescription());
         transferTicket.setCreateDate(new Date());
         transferTicket.setStatus("PENDING");
         transferTicket.setCreateEmployee(employee);
-        Date importDate = createTransferTicketDto.getImportDate();
+        Date importDate = importTicketDto.getImportDate();
         transferTicket.setTransportDate(importDate);
 
         if(createDebtDto!=null){
@@ -85,7 +88,7 @@ public class TransferTicketService {
                 double area = slotWithBin.getArea();
                 Slot slot = slotRepository.findById(slotId).orElseThrow(()->new NotFoundException("Slot does not exist"));
                 if(slot.getCapacity()-slot.getContaining()<area){
-                    throw new SlotOverContaining(area+ " m2 is overload the left capacity of slot: " + slot.getName());
+                    throw new SlotAreaMismatchException(area+ " m2 is overload the left capacity of slot: " + slot.getName());
                 }
                 binSlot.setSlot(slot);
                 binSlot.setWeight(slotWithBin.getWeight());
@@ -108,7 +111,7 @@ public class TransferTicketService {
         }).collect(Collectors.toList());
 
         transferTicket.setBins(bins);
-        Partner partner = partnerRepository.findById(createTransferTicketDto.getProviderId()).orElseThrow(()->new NotFoundException("Partner does not exist"));
+        Partner partner = partnerRepository.findById(importTicketDto.getProviderId()).orElseThrow(()->new NotFoundException("Partner does not exist"));
         transferTicket.setPartner(partner);
         transferTicketRepository.save(transferTicket);
         return "New Import Transfer Ticket created with id: " + transferTicket.getId();
@@ -132,7 +135,7 @@ public class TransferTicketService {
 
             Warehouse warehouse=slot.getRow().getMap().getWarehouse();
             if(slot.getCapacity()-slot.getContaining()<area){
-                    throw new SlotOverContaining(area+ " m2 is overload the left capacity of slot: " + slot.getName());
+                    throw new SlotAreaMismatchException(area+ " m2 is overload the left capacity of slot: " + slot.getName());
             }
             else {
                 slot.setContaining(slot.getContaining()+area);
@@ -182,5 +185,90 @@ public class TransferTicketService {
 
         long count= ticketDaoImpl.countTotalTicketWithFilterAndPagination(pageIndex, pageSize, name, type, status);
         return new PageDto(Math.ceil((double)count/pageSize),count,pageIndex,transferTicketWithBinDtos);
+    }
+    @Transactional
+    public String createExportTicket(CreateExportTransferTicketDto exportTicketDto){
+        
+        List<CreateExportBinDto> exportBinsDto = exportTicketDto.getExportBinDto();
+        CreateDebtDto createDebtDto =exportTicketDto.getDebtDto();
+        Employee employee = EmployeeDetailService.getCurrentUserDetails();
+        employee= employeeRepository.findById(employee.getId()).orElseThrow(()->new NotFoundException("No employee found"));
+        
+        TransferTicket transferTicket = new TransferTicket();
+        transferTicket.setType("EXPORT");
+        transferTicket.setName(exportTicketDto.getName());
+        transferTicket.setDescription(exportTicketDto.getDescription());
+        transferTicket.setCreateDate(new Date());
+        transferTicket.setStatus("PENDING");
+        transferTicket.setCreateEmployee(employee);
+        Date exportDate = exportTicketDto.getExportDate();
+        transferTicket.setTransportDate(exportDate);
+
+        if(createDebtDto!=null){
+            Debt debt = new Debt();
+            debt.setAmount(createDebtDto.getValue());
+            debt.setNote(createDebtDto.getDescription());
+            debt.setCreateDate(new Date());
+            debt.setDueDate(createDebtDto.getDueDate());
+            debt.setName(createDebtDto.getName());
+            transferTicket.addDebt(debt);
+        }
+
+        List<Bin> bins=exportBinsDto.stream().map(binDto->{
+            Bin bin = new Bin();
+            Quality quality = qualityRepository.findById(binDto.getQualityId()).orElseThrow(()->new NotFoundException("Quality does not exist"));
+            bin.setQuality(quality);
+            bin.setTransferTicket(transferTicket);
+            bin.setDocument(binDto.getNote());
+            bin.setExportDate(exportDate);
+            bin.setPrice(binDto.getPrice());
+            bin.setPackageType(binDto.getPackageType());
+            List<BinBin> binBins =new ArrayList<>();
+
+            AtomicReference<Double> binWeight = new AtomicReference<>(0.0);
+            binDto.getImportBinWithSlot().stream().forEach(slotWithImportBin -> {
+                BinBin binBin = new BinBin();
+                Integer slotId=slotWithImportBin.getSlotId();
+                Integer binId = slotWithImportBin.getBin().getId();
+                double takenArea = slotWithImportBin.getTakenArea();
+                double takenWeight = slotWithImportBin.getTakenWeight();
+                Bin importBin = binDaoImpl.findBinInSlotBySlotIdAndBinId(binId,slotId).orElseThrow(()->new NotFoundException("Slot: "+slotId+" does not contains bin: "+binId));
+                BinSlot binSlot=importBin.getBinSlot().get(0);
+                Slot containingSlot=binSlot.getSlot();
+
+                if(binSlot.getArea()<takenArea){
+                    throw new SlotAreaMismatchException(takenArea+ " m2 is larger the current area of bin in slot: " + containingSlot.getName());
+                }
+
+                double importBinAreaInSlot=binSlot.getArea();
+                binSlot.setWeight(binSlot.getWeight()-takenWeight);
+                binSlot.setArea(importBinAreaInSlot-takenArea);
+                binWeight.updateAndGet(v -> v + takenWeight);
+
+                binBin.setImportBin(importBin);
+                binBin.setExportBin(bin);
+                binBins.add(binBin);
+            });
+
+            if(binDto.getWeight()!=binWeight.get()) {
+                throw new BinWeightMismatchException();
+            }else{
+                bin.setWeight(binDto.getWeight());
+                bin.setLeftWeight(binDto.getWeight());
+            }
+            
+
+            bin.setImportBins(binBins);
+
+            return bin;
+
+        }).collect(Collectors.toList());
+
+        transferTicket.setBins(bins);
+        Partner partner = partnerRepository.findById(exportTicketDto.getCustomerId()).orElseThrow(()->new NotFoundException("Partner does not exist"));
+        transferTicket.setPartner(partner);
+        transferTicketRepository.save(transferTicket);
+
+        return "New Export Transfer Ticket created with id: " + transferTicket.getId();
     }
 }
