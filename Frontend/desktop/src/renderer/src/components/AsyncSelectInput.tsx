40e0efc1550e952cdf158794ac6bb2c6c8c871f7
TheRoamer-2901 <dangkhoa29012002@gmail.com>
2024-05-23 15:06:24 +0700

    update dropdown select close on blur

--- Frontend/desktop/src/renderer/src/components/AsyncSelectInput.tsx
+++ Frontend/desktop/src/renderer/src/components/AsyncSelectInput.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import { useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { ListSkeleton } from '@renderer/components';
+import { cn } from '@renderer/utils/util';
 
 type AsyncSelectInputProps = {
     selectedValue: any;
@@ -11,6 +12,7 @@ type AsyncSelectInputProps = {
     icon?: ReactNode;
     asyncSelectorCallback: () => Promise<any>;
     onSelect: (value: any) => void;
+    bg?: string;
 };
 
 const AsyncSelectInput = ({
@@ -19,7 +21,8 @@ const AsyncSelectInput = ({
     label,
     icon,
     asyncSelectorCallback,
-    onSelect
+    onSelect,
+    bg = 'bg-white'
 }: AsyncSelectInputProps) => {
     const [expanded, setExpanded] = useState(false);
     // const openSelect = () => setExpanded(true);
@@ -33,7 +36,7 @@ const AsyncSelectInput = ({
         closeSelect();
     };
 
-    const { data, refetch, isFetching } = useQuery({
+    const { data, refetch, isRefetching, isFetching } = useQuery({
         enabled: false,
         queryKey: [label],
         queryFn: asyncSelectorCallback
@@ -42,13 +45,24 @@ const AsyncSelectInput = ({
     return (
         <div
             className="relative w-full"
+            tabIndex={0}
             onBlur={() => {
-                alert('close');
+                closeSelect();
             }}
         >
             {/* selected option */}
-            <div className="relative flex items-center justify-between bg-gray-50 border border-sky-800 text-gray-900 text-sm rounded-lg  w-full px-4 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-                <div className="bg-gray-50 flex items-center gap-2 absolute px-2 -top-3 left-2">
+            <div
+                className={cn(
+                    'relative flex items-center justify-between border border-sky-800 text-sky-800 text-sm rounded-lg  w-full px-4 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white',
+                    bg
+                )}
+            >
+                <div
+                    className={cn(
+                        'flex items-center gap-2 absolute px-2 -top-3 left-2',
+                        bg
+                    )}
+                >
                     {icon}
                     <p className="text-sm font-semibold text-sky-800">
                         {placeHolder}
@@ -62,13 +76,12 @@ const AsyncSelectInput = ({
             </div>
 
             {/* options */}
-            {expanded && isFetching ? (
-                <ListSkeleton />
-            ) : (
-                expanded &&
-                data?.length > 0 && (
-                    <ul className="absolute top-10 z-50 overflow-hidden max-h-[170px] overflow-y-scroll mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-                        {data?.map((option, i) => (
+            {expanded && (
+                <ul className="absolute top-10 z-50 overflow-hidden max-h-[170px] overflow-y-scroll mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                    {isFetching || isRefetching ? (
+                        <ListSkeleton />
+                    ) : data?.length > 0 ? (
+                        data?.map((option, i) => (
                             <li
                                 className="cursor-pointer py-2 text-center text-sm hover:bg-slate-200 hover:font-semibold"
                                 key={i}
@@ -76,9 +89,13 @@ const AsyncSelectInput = ({
                             >
                                 {option?.name ?? option}
                             </li>
-                        ))}
-                    </ul>
-                )
+                        ))
+                    ) : (
+                        <li className="cursor-pointer py-2 text-center text-sm hover:bg-slate-200 hover:font-semibold">
+                            Chưa có lựa chọn nào
+                        </li>
+                    )}
+                </ul>
             )}
         </div>
     );
